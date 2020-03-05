a03eb42eff3c6c4e43df15011d393c597ebc1272
Cody Hammock <hammock@tacc.utexas.edu>
2020-03-05 11:22:49 -0600

    Replace index with HTML content

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return """<!DOCTYPE html>
+<html>
+        <head><title>Tapis Exporter</title></head>
+        <body>
+                <h1>Tapis Exporter</h1>
+                <p><a href='/metrics'>Metrics</a></p>
+        </body>
+</html>"""
 
 @app.route('/metrics')
 def metrics():
